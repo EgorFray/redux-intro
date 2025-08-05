b8649fa2a6370d2c9593b2e5573e6edba45819e2
EgorFray <fatamorganaa933@gmail.com>
2025-08-05 21:13:46 +0300

    Making an API call with Redux Thunks

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,18 +2,25 @@ const initialStateAccount = {
 	balance: 0,
 	loan: 0,
 	loanPurpose: "",
+	isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
 	switch (action.type) {
 		case "account/deposit": {
-			return { ...state, balance: state.balance + action.payload };
+			return { ...state, balance: state.balance + action.payload, isLoading: false };
 		}
 		case "account/withdraw": {
 			return { ...state, balance: state.balance - action.payload };
 		}
 		// LATER
 		case "account/requestLoan": {
+			return {
+				...state,
+				loan: action.payload.amount,
+				loanPurpose: action.payload.purpose,
+				balance: state.balance + action.payload.amount,
+			};
 		}
 		case "account/payLoan": {
 			return {
@@ -23,21 +30,34 @@ export default function accountReducer(state = initialStateAccount, action) {
 				balance: state.balance - state.loan,
 			};
 		}
+		case "account/convertingCurrency": {
+			return { ...state, isLoading: true };
+		}
 
 		default:
 			return state;
 	}
 }
 
-export function deposit(amount) {
-	return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+	if (currency === "USD") return { type: "account/deposit", payload: amount };
+	return async function (dispatch, getState) {
+		dispatch({ type: "account/convertingCurrency" });
+		const res = await fetch(
+			`https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD&amount=${amount}`
+		);
+		const data = await res.json();
+		const converted = data.rates.USD;
+
+		dispatch({ type: "account/deposit", payload: converted });
+	};
 }
 export function withdraw(amount) {
 	return { type: "account/withdraw", payload: amount };
 }
 export function requestLoan(amount, purpose) {
 	return {
-		type: "account/requstLoan",
+		type: "account/requestLoan",
 		payload: {
 			amount,
 			purpose,
